3cce464e7867a5c8b5a74cdc46e88c1b21bb23ef
nuke <giovanni.celia1995@gmail.com>
2024-10-21 22:40:42 +0200

    first commit

---
+++ src/main/java/giovanni/celia/storage_handler/StorageHandlerApplication.java
@@ -0,0 +1,13 @@
+package giovanni.celia.storage_handler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorageHandlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/giovanni/celia/storage_handler/StorageHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package giovanni.celia.storage_handler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageHandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
